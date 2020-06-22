d0cd9e3b3264344abfd27b33c5468c6fe2507d2c
av <arcovoltaico@gmail.com>
2020-06-22 20:47:52 +0100

    flo not working on build prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FloDashModule} from "@flosportsinc/ng-media-source-extensions/dash";
+import {FloMseModule} from "@flosportsinc/ng-media-source-extensions";
+import {FloHlsModule} from "@flosportsinc/ng-media-source-extensions/hls";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FloDashModule,
+    FloMseModule,
+    FloHlsModule, // without config overrides
+    FloHlsModule.config({
+      // floConfig: {
+      //   selfHeal: true // attempts to fix errors automatically
+      // },
+      hlsConfig: {
+        capLevelToPlayerSize: true, // this module defaults to responsive renditions
+        startLevel: -1 // this module defaults to auto start leveling
+      } // See: https://github.com/video-dev/hls.js/blob/master/docs/API.md#fine-tuning
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
